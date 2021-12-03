a9e0c4f4c358c7998b09fc2a7bca2ac89dd886ca
Sarra Sakouhi <sarra.sakouhi@neoxam.com>
2021-12-03 13:29:25 +0100

    add depencencies log4j and testU

--- src/main/java/tn/esprit/spring/controller/RestControlTimesheet.java
+++ src/main/java/tn/esprit/spring/controller/RestControlTimesheet.java
@@ -3,6 +3,7 @@ package tn.esprit.spring.controller;
 import java.util.Date;
 import java.util.List;
 
+import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,59 +22,90 @@ import tn.esprit.spring.services.ITimesheetService;
 @RestController
 public class RestControlTimesheet {
 
+	private static final Logger L = Logger.getLogger(RestControlTimesheet.class);
 	@Autowired
 	IEmployeService iemployeservice;
 	@Autowired
 	IEntrepriseService ientrepriseservice;
 	@Autowired
 	ITimesheetService itimesheetservice;
-	
+
 	// http://localhost:8081/SpringMVC/servlet/ajouterMission
-	//{"id":4,"name":"mamission", "description":"c ma mission"}
+	// {"id":4,"name":"mamission", "description":"c ma mission"}
 	@PostMapping("/ajouterMission")
 	@ResponseBody
 	public int ajouterMission(@RequestBody Mission mission) {
-		itimesheetservice.ajouterMission(mission);
-		return mission.getId();
+		try {
+			itimesheetservice.ajouterMission(mission);
+			return mission.getId();
+		} catch (Exception e) {
+			L.error("there is an error while executing : ajouterMission() : " + e);
+		}
+		return 0;
 	}
 
 	// http://localhost:8081/SpringMVC/servlet/affecterMissionADepartement/4/4
-	@PutMapping(value = "/affecterMissionADepartement/{idmission}/{iddept}") 
-	public void affecterMissionADepartement(@PathVariable("idmission") int missionId, @PathVariable("iddept") int depId) {
-		itimesheetservice.affecterMissionADepartement(missionId, depId);
+	@PutMapping(value = "/affecterMissionADepartement/{idmission}/{iddept}")
+	public void affecterMissionADepartement(@PathVariable("idmission") int missionId,
+			@PathVariable("iddept") int depId) {
+		try {
+			itimesheetservice.affecterMissionADepartement(missionId, depId);
+		} catch (Exception e) {
+			L.error("there is an error while executing : affecterMissionADepartement() : " + e);
+		}
 
 	}
-	
+
 	// http://localhost:8081/SpringMVC/servlet/ajouterTimesheet
-    //{"missionId":1,"employeId":2,"dateDebut":"2020-03-01","dateFin":"2021-03-01"}
-	
+	// {"missionId":1,"employeId":2,"dateDebut":"2020-03-01","dateFin":"2021-03-01"}
+
 	@PostMapping("/ajouterTimesheet/idmission/idemp/dated/datef")
 	@ResponseBody
-	public void ajouterTimesheet(@PathVariable("idmission") int missionId, @PathVariable("idemp") int employeId, @PathVariable("dated") Date dateDebut,@PathVariable("datef") Date dateFin) {
-		itimesheetservice.ajouterTimesheet(missionId, employeId, dateDebut, dateFin);
+	public void ajouterTimesheet(@PathVariable("idmission") int missionId, @PathVariable("idemp") int employeId,
+			@PathVariable("dated") Date dateDebut, @PathVariable("datef") Date dateFin) {
+
+		try {
+			itimesheetservice.ajouterTimesheet(missionId, employeId, dateDebut, dateFin);
+		} catch (Exception e) {
+			L.error("there is an error while executing : affecterMissionADepartement() : " + e);
+		}
 
 	}
 
 	// http://localhost:8081/SpringMVC/servlet/affecterMissionADepartement/4/4
-	@PutMapping(value = "/validerTimesheet/{idmission}/{iddept}") 
+	@PutMapping(value = "/validerTimesheet/{idmission}/{iddept}")
 	public void validerTimesheet(int missionId, int employeId, Date dateDebut, Date dateFin, int validateurId) {
-		itimesheetservice.validerTimesheet(missionId, employeId, dateDebut, dateFin, validateurId);
+		try {
+			itimesheetservice.validerTimesheet(missionId, employeId, dateDebut, dateFin, validateurId);
+		} catch (Exception e) {
+			L.error("there is an error while executing : validerTimesheet() : " + e);
+		}
 
 	}
-	
+
 	// URL : http://localhost:8081/SpringMVC/servlet/findAllMissionByEmployeJPQL/1
-    @GetMapping(value = "findAllMissionByEmployeJPQL/{idemp}")
-    @ResponseBody
+	@GetMapping(value = "findAllMissionByEmployeJPQL/{idemp}")
+	@ResponseBody
 	public List<Mission> findAllMissionByEmployeJPQL(@PathVariable("idemp") int employeId) {
-
-		return itimesheetservice.findAllMissionByEmployeJPQL(employeId);
+		try {
+			return itimesheetservice.findAllMissionByEmployeJPQL(employeId);
+		} catch (Exception e) {
+			L.error("there is an error while executing : findAllMissionByEmployeJPQL() : " + e);
+		}
+		return null;
+		
 	}
 
-    // URL : http://localhost:8081/SpringMVC/servlet/getAllEmployeByMission/1
-    @GetMapping(value = "getAllEmployeByMission/{idmission}")
-    @ResponseBody
+	// URL : http://localhost:8081/SpringMVC/servlet/getAllEmployeByMission/1
+	@GetMapping(value = "getAllEmployeByMission/{idmission}")
+	@ResponseBody
 	public List<Employe> getAllEmployeByMission(@PathVariable("idmission") int missionId) {
-
-		return itimesheetservice.getAllEmployeByMission(missionId);
+		try {
+			L.info("In getAllEmployeByMission() : ");
+			return itimesheetservice.getAllEmployeByMission(missionId);
+		} catch (Exception e) {
+			L.error("there is an error while executing : getAllEmployeByMission() : " + e);
+		}
+		return null;
 	}
 }
